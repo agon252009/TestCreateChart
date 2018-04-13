1d73979fab61b1baee98252a4a940782468a8dd7
Gonda, Alberto <alberto.gonda@advancedbionics.com>
2018-04-13 13:40:07 -0700

    First Checkin

---
+++ TestCreateChart/MyChart.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace TestCreateChart
+{
+    /// <summary>
+    /// Interaction logic for MyChart.xaml
+    /// </summary>
+    public partial class MyChart : UserControl
+    {
+        public MyChart()
+        {
+            InitializeComponent();
+        }
+
+        public double MinYValue { get; set; }
+
+        public double MaxYValue { get; set; }
+
+        public double MinXValue { get; set; }
+
+        public double MaxXValue { get; set; }
+
+        public int YAxisHeaderWidth { get; set; } = 50;
+
+        public int XAxisHeaderHeight { get; set; } = 25;
+    }
+}
