16467025033fa5105a2a0e982edea851da8cc960
Gonda, Alberto <alberto.gonda@advancedbionics.com>
2018-04-25 16:39:15 -0700

    I dont even know

---
+++ TestCreateChart/ChartAxisItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TestCreateChart
+{
+    public class ChartAxisItem : Control
+    {
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(ChartAxisItem), new PropertyMetadata(0.0));
+        
+        static ChartAxisItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ChartAxisItem), new FrameworkPropertyMetadata(typeof(ChartAxisItem)));
+        }
+    }
+}
